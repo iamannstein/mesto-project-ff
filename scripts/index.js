5d940ed7cfbe274eeb367d8fc11f8560ad680823
Anna Stein <iamannstein@gmail.com>
2024-02-29 00:05:50 +0100

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const cardsContainer = content.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const deleteButton = cardTemplate.querySelector('.card__delete-button')
+const createNewCard = content.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+initialCards.forEach((el) => {
+  createCard(el, deleteCard);
+});
 
-// @todo: Функция удаления карточки
+function deleteCard(e) {
+  e.target.parentElement.remove();
+} 
 
-// @todo: Вывести карточки на страницу
+function createCard(element, deleteCard) {
+
+  const cardElement =  cardTemplate.querySelector('.places__item').cloneNode('true');
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  cardsContainer.append(cardElement);
+
+  return cardElement;
+
+}
+
+
+createNewCard.addEventListener('click', function() {
+
+})
